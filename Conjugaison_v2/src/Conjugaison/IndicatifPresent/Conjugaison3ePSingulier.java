17c3d7c87c3b9f1b12165d99bc6a15ac69b8e35a
Tlatzer <47251962+Tlatzer@users.noreply.github.com>
2023-03-21 15:27:17 +0100

    Update Conjugaison3ePSingulier.java

--- Conjugaison_v2/src/Conjugaison/IndicatifPresent/Conjugaison3ePSingulier.java
+++ Conjugaison_v2/src/Conjugaison/IndicatifPresent/Conjugaison3ePSingulier.java
@@ -1,1 +1,12 @@
+package Conjugaison.IndicatifPresent;
 
+import Verbes.Verbe;
+
+public class Conjugaison3ePSingulier {
+
+    public String conjuger(Verbe v) {
+
+        return v.conjugue3emePersonneSingulier();
+
+    }
+}
