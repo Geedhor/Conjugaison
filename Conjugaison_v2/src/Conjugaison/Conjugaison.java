6b3c4e7cfff39a979e4e62879abcc836dde253c0
Tlatzer <47251962+Tlatzer@users.noreply.github.com>
2023-03-23 13:25:26 +0100

    Update Conjugaison.java

--- Conjugaison_v2/src/Conjugaison/Conjugaison.java
+++ Conjugaison_v2/src/Conjugaison/Conjugaison.java
@@ -9,8 +9,9 @@ public abstract class Conjugaison {
 			conjuguer1erePersonneSingulier(v) + '\n' +
 			conjuguer2emePersonneSingulier(v) + '\n' +
 			conjuguer3emePersonneSingulier(v) + '\n' +
-			conjuguer1erePersonnePluriel(v) + '\n'
-		;
+			conjuguer1erePersonnePluriel(v) + '\n' + 
+			conjuguer2emePersonnePluriel(v) + '\n' + 
+			conjuguer3emePersonnePluriel(v);
 	}
 
 	public abstract String conjuguer1erePersonneSingulier(Verbe v);
@@ -20,5 +21,9 @@ public abstract class Conjugaison {
 	public abstract String conjuguer3emePersonneSingulier(Verbe v);
 
 	public abstract String conjuguer1erePersonnePluriel(Verbe v);
+	
+	public abstract String conjuguer2emePersonnePluriel(Verbe v);
+	
+	public abstract String conjuguer3emePersonnePluriel(Verbe v);
 	
 }
