36ec0bdb016838badcef5a23ca0e9aeb457fbfa9
Tlatzer <47251962+Tlatzer@users.noreply.github.com>
2023-03-24 08:27:34 +0100

    Update ConjugaisonIndicatifFutur.java

--- Conjugaison_v2/src/Conjugaison/Indicatif/ConjugaisonIndicatifFutur.java
+++ Conjugaison_v2/src/Conjugaison/Indicatif/ConjugaisonIndicatifFutur.java
@@ -1,1 +1,37 @@
+package Conjugaison.Indicatif;
 
+import Conjugaison.Conjugaison;
+import Verbes.Verbe;
+
+public class ConjugaisonIndicatifFutur extends Conjugaison {
+
+	public String conjuguer1erePersonneSingulier(Verbe v) {
+		//return v.conjugue1erePersonneSingulierIndicatifFutur();
+    return "";
+	}
+
+	public String conjuguer2emePersonneSingulier(Verbe v) {
+		//return v.conjugue2emePersonneSingulierIndicatifFutur();
+    return "";
+	}
+
+	public String conjuguer3emePersonneSingulier(Verbe v) {
+		//return v.conjugue3emePersonneSingulierIndicatifFutur();
+    return "";
+	}
+
+	public String conjuguer1erePersonnePluriel(Verbe v) {
+		//return v.conjugue1erePersonnePlurielIndicatifFutur();
+    return "";
+	}
+	
+	public String conjuguer2emePersonnePluriel(Verbe v) {
+		//return v.conjugue2emePersonnePlurielIndicatifFutur();
+    return "";
+	}
+	
+	public String conjuguer3emePersonnePluriel(Verbe v) {
+		//return v.conjugue3emePersonnePlurielIndicatifFutur();
+    return "";
+	}
+}
